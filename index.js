032d5030973169f75c6af952a87132690a30f726
gnwncpta <gnwncpta@gmail.com>
2021-07-29 22:36:41 +0700

    update code

--- index.js
+++ index.js
@@ -1,58 +1,82 @@
+const prompt = require('prompt');
+prompt.start();
+
+const chalk = require('chalk');
 const getNumbers = require('./getNumbers.js');
 const showCalc = require('./showCalc.js');
 const showResult = require('./showResult.js');
-let calcNumbers = '10+10+5*2'; // the main numbers
+let calcNumbers = ''; // the main numbers
 
-// loopers variable is to take the operators from calcNumbers
-// and then make the loopers for looping point
-let loopers = calcNumbers.split('').map(item => {
-	if(item == '*'){
-		return '*'
-	} else if(item == '/'){
-		return '/'
-	} else if(item == '+'){
-		return '+'
-	} else if(item == '-'){
-		return '-'
-	}
-}).join('').split('');
+prompt.get(['numbers'], (err, result) => {
+	calcNumbers = result.numbers;
+	console.log(`You input: ${calcNumbers}\n`);
+	start();
+});
 
-for(let i = 0; i < loopers.length; i++){
 
-	if(calcNumbers.indexOf('*') != -1){
+function start(){
 
-		// showCalc('let', 'calcNumbers', calcNumbers );
-		const { leftNum, rightNum, result } = getNumbers('*', calcNumbers);
-		calcNumbers = calcNumbers.replace(`${leftNum}*${rightNum}`, result);
+	// loopers variable is to take the operators from calcNumbers
+	// and then make the loopers for looping point
+	let loopers = calcNumbers.split('').map(item => {
+		if(item == '*'){
+			return '*'
+		} else if(item == '/'){
+			return '/'
+		} else if(item == '+'){
+			return '+'
+		} else if(item == '-'){
+			return '-'
+		}
+	}).join('').split('');
 
-		showResult(leftNum, rightNum, '*');
 
-	} else if(calcNumbers.indexOf('/') != -1){
+	for(let i = 0; i < loopers.length; i++){
 
-		// showCalc('let', 'calcNumbers', calcNumbers );
-		const { leftNum, rightNum, result } = getNumbers('/', calcNumbers);
-		calcNumbers = calcNumbers.replace(`${leftNum}/${rightNum}` ,result);
+		if(calcNumbers.indexOf('*') != -1){
 
-		showResult(leftNum, rightNum, '/');
+			showCalc('let', 'calcNumbers', calcNumbers );
+			const { leftNum, rightNum, result } = getNumbers('*', calcNumbers);
+			calcNumbers = calcNumbers.replace(`${leftNum}*${rightNum}`, result);
 
-	} else if(calcNumbers.indexOf('+') != -1){
+			showResult(leftNum, rightNum, '*');
 
-		// showCalc('let', 'calcNumbers', calcNumbers );
-		const { leftNum, rightNum, result } = getNumbers('+', calcNumbers);
+		} else if(calcNumbers.indexOf('/') != -1){
 
-		// manipulasi string yang telah dikalkulasi menjadi hasil kalkulasi
-		calcNumbers = calcNumbers.replace(`${leftNum}+${rightNum}`, result);
+			showCalc('let', 'calcNumbers', calcNumbers );
+			const { leftNum, rightNum, result } = getNumbers('/', calcNumbers);
+			calcNumbers = calcNumbers.replace(`${leftNum}/${rightNum}` ,result);
 
-		showResult(leftNum, rightNum, '+');	
+			showResult(leftNum, rightNum, '/');
 
-	} else if(calcNumbers.indexOf('-') != -1){
+		} else if(calcNumbers.indexOf('+') != -1){
 
-		// showCalc('let', 'calcNumbers', calcNumbers );
-		const { leftNum, rightNum, result } = getNumbers('-', calcNumbers);
-		calcNumbers - calcNumbers.replace(`${leftNum}-${rightNum}`, result);
+			showCalc('let', 'calcNumbers', calcNumbers );
+			const { leftNum, rightNum, result } = getNumbers('+', calcNumbers);
 
-		showResult(leftNum, rightNum, '-');
+			// manipulasi string yang telah dikalkulasi menjadi hasil kalkulasi
+			calcNumbers = calcNumbers.replace(`${leftNum}+${rightNum}`, result);
+
+			showResult(leftNum, rightNum, '+');	
+
+		} else if(calcNumbers.indexOf('-') != -1){
+
+			showCalc('let', 'calcNumbers', calcNumbers );
+			const { leftNum, rightNum, result } = getNumbers('-', calcNumbers);
+			calcNumbers = calcNumbers.replace(`${leftNum}-${rightNum}`, result);
+
+			showResult(leftNum, rightNum, '-');
+
+		}
 
 	}
 
 }
+
+
+
+// console.log(`Result: ${ chalk.magenta(`${calcNumbers}`) }`);
+
+
+
+
